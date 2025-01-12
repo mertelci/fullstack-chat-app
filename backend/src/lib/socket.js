e8f5b2205c82d1b95fb6292b607805bdd349d681
mertelci <mertelci111@gmail.com>
2025-01-12 21:10:15 +0300

    socket.js fix

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -2,32 +2,24 @@ import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 
-
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173"]
-
-
-    }
+        origin: ["http://localhost:5173"],
+    },
 });
 
 export function getReceiverSocketId(userId) {
-
     return userSocketMap[userId];
-
 }
 
-
-
-
-/* STORE ONLINE USERS */
-const userSocketMap = {}
+// used to store online users
+const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
-    console.log("User connected", socket.id)
+    console.log("A user connected", socket.id);
 
     const userId = socket.handshake.query.userId;
     if (userId) userSocketMap[userId] = socket.id;
@@ -35,16 +27,11 @@ io.on("connection", (socket) => {
     // io.emit() is used to send events to all the connected clients
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-
-
-
     socket.on("disconnect", () => {
-
-        console.log("User disconnected", socket.id)
-
+        console.log("A user disconnected", socket.id);
         delete userSocketMap[userId];
         io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    })
-})
+    });
+});
 
-export { io, app, server }
+export { io, app, server };
